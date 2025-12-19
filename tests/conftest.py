0adc115951564a62716879d2f2ead7ea4b4ccf7b
kmichoud <kmichoud@protonmail.com>
2025-12-19 11:28:00 +0100

    fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,11 @@ from strong_password import StrongPassword
 # We generate a random password as a fixture that we can use in test
 @pytest.fixture
 def random_password():
-    random_p = StrongPassword(length=12, type_p="random").generate()
+    random_p = StrongPassword(length=12, type_p="random".upper()).generate()
     return random_p
 
 
 @pytest.fixture
 def memorable_password():
-    mem_p = StrongPassword(length=5, type_p="memorable").generate()
+    mem_p = StrongPassword(length=5, type_p="memorable".upper()).generate()
     return mem_p
